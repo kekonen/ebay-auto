543ab517f961b64fc09004c611d4677e807eee0c
Kekonen <daniil.naumetc@gmail.com>
2019-01-24 23:16:00 +0000

    save name feature

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-scrapy crawl cars -o '/media/nvidia/card/spider/cars.json'
+KEK=`python3 -c "import datetime as dt;n=dt.datetime.now();print('{0:%d-%m-%ycars.json}'.format(n))"`
+
+lol="/media/nvidia/card/spider/$KEK"
+echo run:scrapy crawl cars -o $lol
+
+scrapy crawl cars -o $lol
+
