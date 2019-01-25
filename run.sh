117dc999165e69bbca17e024153584d7b511baa0
Kekonen <daniil.naumetc@gmail.com>
2019-01-25 08:48:10 +0000

    run for cron

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-KEK=`python3 -c "import datetime as dt;n=dt.datetime.now();print('{0:%d-%m-%ycars.json}'.format(n))"`
+KEK=`python3 -c "import datetime as dt;n=dt.datetime.now();print('{0:%H_%d-%m-%ycars.json}'.format(n))"`
 
 lol="/media/nvidia/card/spider/$KEK"
-echo run:scrapy crawl cars -o $lol
+echo run: scrapy crawl cars -o $lol
 
 scrapy crawl cars -o $lol
 
